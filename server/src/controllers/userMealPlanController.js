28bfb6fb4eb0900ca8cf0fe348013ac66858c98d
Osman Demiröz <osmanndemirozz@gmail.com>
2025-05-12 13:28:46 +0300

    feat: add getUserMealPlan function to retrieve meal plans by user ID

--- server/src/controllers/userMealPlanController.js
+++ server/src/controllers/userMealPlanController.js
@@ -1,6 +1,6 @@
 const userMealPlanService = require('../services/userMealPlanService');
 
-const generateUserMealPlan = async (req, res) => {
+const generateUserMealPlan = async(req, res) => {
     try {
         const { uid, gid } = req.body;
 
@@ -11,11 +11,33 @@ const generateUserMealPlan = async (req, res) => {
         const result = await userMealPlanService.generateMealPlanForUser(uid, gid);
         res.status(200).json(result);
     } catch (err) {
-        console.error('Error generating meal plan:', err);
+        console.error('[generateUserMealPlan] Error generating meal plan:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+const getUserMealPlan = async(req, res) => {
+    try {
+        const { uid } = req.params;
+
+        if (!uid) {
+            return res.status(400).json({ error: 'User ID is required' });
+        }
+
+        const mealPlan = await userMealPlanService.getMealPlanForUser(uid);
+
+        if (!mealPlan) {
+            return res.status(404).json({ error: 'Meal plan not found for this user' });
+        }
+
+        res.status(200).json(mealPlan);
+    } catch (err) {
+        console.error('[getUserMealPlan] Error fetching meal plan:', err);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
 
 module.exports = {
-    generateUserMealPlan
+    generateUserMealPlan,
+    getUserMealPlan
 };
